1626060debebec74f56348bff1a719a37bc5a37f
Eduardo Ibarra <eduardoibarra904@gmail.com>
2021-01-20 14:34:04 -0600

    Added cupertino bottom bar

--- lib/pueblos_magicos.dart
+++ lib/pueblos_magicos.dart
@@ -12,30 +12,40 @@ class PueblosMagicos extends StatefulWidget {
 }
 
 class _PueblosMagicos extends State<PueblosMagicos> {
+  int indexTap = 0;
   final List<Widget> widgetsChildren = [PlaceDetail(), Search(), Profile()];
-  void onTabTapped(int index) {}
+  void onTapTapped(int index) {
+    setState(() {
+      indexTap = index;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      body: widgetsChildren[indexTap],
       bottomNavigationBar: Theme(
           data: Theme.of(context).copyWith(
             canvasColor: Colors.white,
             primaryColor: Colors.purple,
           ),
-          child: BottomNavigationBar(items: [
-            BottomNavigationBarItem(
-              icon: Icon(Icons.home),
-              label: '',
-            ),
-            BottomNavigationBarItem(
-              icon: Icon(Icons.search),
-              label: '',
-            ),
-            BottomNavigationBarItem(
-              icon: Icon(Icons.person),
-              label: '',
-            )
-          ])),
+          child: BottomNavigationBar(
+              onTap: onTapTapped,
+              currentIndex: indexTap,
+              items: [
+                BottomNavigationBarItem(
+                  icon: Icon(Icons.home),
+                  label: '',
+                ),
+                BottomNavigationBarItem(
+                  icon: Icon(Icons.search),
+                  label: '',
+                ),
+                BottomNavigationBarItem(
+                  icon: Icon(Icons.person),
+                  label: '',
+                )
+              ])),
     );
   }
 }
